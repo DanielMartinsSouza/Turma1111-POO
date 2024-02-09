cc4b243886aa4d9e028a20409afac1704e56f1e8
Daniel Martins de Souza <danielmartsouza@gmail.com>
2024-02-09 19:43:16 -0300

    Iniciando aula do dia 09/02

--- src/Aula0902/Contato.java
+++ src/Aula0902/Contato.java
@@ -1,4 +1,15 @@
 package Aula0902;
 
 public class Contato {
+    String nome;
+    String telefone;
+    String email;
+
+    public Contato(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+
 }
--- src/Aula0902/Main.java
+++ src/Aula0902/Main.java
@@ -1,4 +1,7 @@
 package Aula0902;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
